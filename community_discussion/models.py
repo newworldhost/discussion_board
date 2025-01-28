c8dc2de210f9ea5ca6b949eb95cdb2f15601b4c6
NewWorldHost <jeffries839@gmail.com>
2025-01-28 21:15:53 +0000

    post model created

--- community_discussion/models.py
+++ community_discussion/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+#tuple creation for status field
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
