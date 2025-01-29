e6527a11bacfe7863d10e44d017e1528890bb9d8
NewWorldHost <jeffries839@gmail.com>
2025-01-29 16:08:54 +0000

    models created

--- community_discussion/models.py
+++ community_discussion/models.py
@@ -17,3 +17,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+        name = models.CharField(max_length=100)
+        description = models.TextField(blank=True, null=True)
+    
+def __str__(self):
+            return self.name
+    
+    
+class SubCategory(models.Model):
+        name = models.CharField(max_length=100)
+        description = models.TextField(blank=True, null=True)
+        category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    
+def __str__(self):
+            return self.name
