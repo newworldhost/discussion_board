8d2d592add9713868643118bd28cf84ee001120f
NewWorldHost <jeffries839@gmail.com>
2025-01-30 15:53:57 +0000

    readme update

--- community_discussion/views.py
+++ community_discussion/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Category, SubCategory, Post
 
-# Create your views here.
 class HomePage(TemplateView):
     """
-    Displays home page"
+    Displays home page
     """
     template_name = 'community_discussion/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['subcategories'] = SubCategory.objects.all()
+        context['posts'] = Post.objects.all()
+        return context
