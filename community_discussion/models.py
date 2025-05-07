abe3ce3d5cde1ceebfa00afed548e7ae1a162caa
newworldhost <jeffries839@gmail.com>
2025-05-07 12:03:17 +0100

    refactor: modifiied the code on models.py to become pep8 compliant and removed duplicated code

--- community_discussion/models.py
+++ community_discussion/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -8,24 +9,33 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class SubCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='subcategories', on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.IntegerField(choices=[(0, 'Draft'), (1, 'Published')], default=0)
-    subcategory = models.ForeignKey(SubCategory, related_name='posts', on_delete=models.CASCADE)
+    status = models.IntegerField(
+        choices=[(0, 'Draft'), (1, 'Published')], default=0
+    )
+    subcategory = models.ForeignKey(
+        SubCategory, related_name='posts', on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -36,24 +46,30 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, related_name='comments', on_delete=models.CASCADE, blank=True, null=True)
-    subcategory = models.ForeignKey(SubCategory, related_name='comments', on_delete=models.CASCADE, blank=True, null=True)
-    
+    category = models.ForeignKey(
+        Category, related_name='comments', on_delete=models.CASCADE, blank=True, null=True
+    )
+    subcategory = models.ForeignKey(
+        SubCategory, related_name='comments', on_delete=models.CASCADE, blank=True, null=True
+    )
+
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
 
-def save(self, *_args, **kwargs):
-#automatically set the category and subcategory based on the post
-    if self.post:
-        self.category = self.post.subcategory.category
-        self.subcategory = self.post.subcategory
-    super().save(*args, **kwargs)
-
+    def save(self, *args, **kwargs):
+        # Automatically set the category and subcategory based on the post
+        if self.post:
+            self.category = self.post.subcategory.category
+            self.subcategory = self.post.subcategory
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
