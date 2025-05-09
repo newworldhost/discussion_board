1ada2fe3428d3d259337a5c170d4479f8ed2df1a
newworldhost <jeffries839@gmail.com>
2025-05-09 15:28:59 +0100

    confugure collectstatic for heroku

--- community/settings.py
+++ community/settings.py
@@ -49,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'https://community-discussion-e4f1c07e8633.herokuapp.com/',
